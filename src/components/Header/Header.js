abdf1eb26552a22822a09aa24a8da37dbd087899
Van Quang <49120019+vanquanguetk63@users.noreply.github.com>
2021-07-23 17:30:43 +0700

    Update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import Container from "react-bootstrap/Container";
-import '../Header/Header.css';
+import React from "react";
+import "../Header/Header.css";
+import Image from "react-bootstrap/Image";
+import logo from "../../resources/images/logo.png";
+import { Row, Container, Col, Navbar, Nav } from "react-bootstrap";
 
 const Header = () => {
-    return (
-        <Container className="custom-header">
-            hello
-        </Container>
-    );
+  return (
+    <Container fluid className="custom-header">
+      <Container>
+        <Row>
+          <Col lg={3} className="logo">
+            <Image fluid src={logo} />
+          </Col>
+          <Col lg={8}>
+            <Nav className="custom-header-nav">
+              <Nav.Item>
+                <Nav.Link href="/home">Active</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="link-1">Link</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey="link-2">Link</Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
 };
 
 export default Header;
